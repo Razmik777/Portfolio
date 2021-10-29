498e7fabbd782b5e27c975c23ee28e2db7a77fed
Razmik <79207161+Razmik777@users.noreply.github.com>
2021-10-29 20:37:11 +0300

    Add Portfolio

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+        nextEl: '.portfolio__arrow_right',
+        prevEl: '.portfolio__arrow_left',
+        disabledClass: 'portfolio__arrow_disable',
+    },
+    breakpoints: {
+        480: {
+            grid: {
+                fill: 'row',
+                rows: 1
+            }
+        },
+        768: {
+            slidesPerView: 2,
+            grid: {
+                fill: 'row',
+                rows: 1
+            } 
+        }
+    }
+})
+// Jquery
+
+$('.header__contacts-burger').click(() => {
+    $('.contacts').slideToggle()
+})
+
+$('.present__order-btn').click(() => {
+    $('.page__overlay_modal').fadeIn(400).css('display', 'flex')
+})
+
+$('.modal__close').click(() => {
+    $('.page__overlay_modal').fadeOut(400)
+})
